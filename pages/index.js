56ec0be48d17d6819769a7fedb964fdd62ea50a4
Manomae <89996676+Manomae@users.noreply.github.com>
2026-04-05 12:00:19 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      backgroundColor: '#1a1a1a', 
+      color: 'orange', 
+      height: '100vh', 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      fontFamily: 'sans-serif',
+      textAlign: 'center' 
+    }}>
+      <h1>Meu Portfólio de Anime em Construção! 🚀</h1>
+    </div>
+  )
+}
